331bd2438fa400c0c2e5d2f8114649716e42d5c9
mentat51 <sylvain.fauveau@gmail.com>
2022-01-04 16:42:13 +0100

    j'ai bon (axios) ?

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 import QuoteCard from './components/QuoteCard';
@@ -7,9 +8,10 @@ function App() {
   const [quote, setQuote] = React.useState({});
 
   const changeQuote = () => {
-    fetch('https://simpsons-quotes-api.herokuapp.com/quotes')
+    axios
+    .get('https://simpsons-quotes-api.herokuapp.com/quotes')
     //extract the data from the received response
-    .then((response) => response.json())
+    .then((response) => response.data)
     //use this data to update the state
     .then((data) => {
       console.log(data[0])
